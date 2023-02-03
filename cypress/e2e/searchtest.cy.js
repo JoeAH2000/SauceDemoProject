3b350a4cb83501b6d6f6d63d2b6a6d5dd5a65f3e
JoeAH2000 <joehilton00@gmail.com>
2023-02-03 13:51:21 +0000

    Added search test

---
+++ cypress/e2e/searchtest.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+it('Standard Login', () => {
+    cy.visit('https://www.google.com/')
+
+    cy.get('.jw8mI').scrollTo("bottom")
+
+    cy.get('#L2AGLb > .QS5gu').click()
+
+    cy.get('.gLFyf').type('Fanduel')
+
+    cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click()
+})
